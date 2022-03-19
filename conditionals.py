79facde82a4490afcb6bbe051ebd13256f08c99c
borusevich-li <eborisevich2007@gmail.com>
2022-03-19 22:39:58 +0100

    added first solution

---
+++ conditionals.py
@@ -0,0 +1,18 @@
+n = int(input("Enter a number, please - "))
+
+print("n = " + str(n))
+
+if n <= 0:
+    print("n <= 0")
+elif n >= 10:
+    print("n >= 10")
+elif n >= 5:
+    print("n >= 5")
+elif n >= 3:
+    print("n >=3")
+elif n == 1:
+    print("n ==1")
+elif n == -1:
+    print("n == -1")
+else:
+    print("n equals something different ")
